0ca34563d4effa4a18f693a1cf5683f5173b75f7
ankita615 <140796173+ankita615@users.noreply.github.com>
2025-02-15 10:51:21 +0530

    Add files via upload

---
+++ Information.java
@@ -0,0 +1,13 @@
+
+public class Information {
+
+    public static void main(String[] args) {
+        System.out.println("NAME      MAIL-ID          EMPLOYEE-CODE      PHONE");
+        System.out.println("Ankita    ankita@cse        a04170          45789067");
+        System.out.println("Shreya    shreya@cse        a04171          45879087");
+        System.out.println("Stuti     stuti@cse         a04172          45879099");
+        System.out.println("Tanisha   tanisha@cse       a04173          45879087");
+        System.out.println("Kaya      kaya@cse          a04174          45876890");
+
+    }
+}
